a0e8d5bfb5c35f9fb8b2b0028c4e6aa6d3610310
JohnMartin <johnrothwellmartin3@gmail.com>
2026-05-20 14:47:28 -0400

    Initial Commit

---
+++ LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by Catherine Fratila on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
